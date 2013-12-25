fa1de8cf317c05e823893f2c5381f285b1acd977
ssstrz <codecats@gmail.com>
2013-12-25 21:50:26 +0100

    discovering controller and route

--- src/Ssstrz/TestBundle/Controller/DefaultController.php
+++ src/Ssstrz/TestBundle/Controller/DefaultController.php
@@ -6,8 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name, $surname)
     {
-        return $this->render('SsstrzTestBundle:Default:index.html.twig', array('name' => $name));
+    	$url = $this->generateUrl('ssstrz_test_homepage', array(
+    		'name' => $name,
+    		'surname' => $surname
+    	));
+        return $this->render(
+        		'SsstrzTestBundle:Default:index.html.twig', 
+        		array(
+        				'name' 		=> $name, 
+        				'surname' 	=> $surname, 
+        				'url' 		=> $url
+        		)
+        );
     }
 }
