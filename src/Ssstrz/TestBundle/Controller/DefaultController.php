26cd8cf993ab90e013ec47824a16dc6405594324
ssstrz <codecats@gmail.com>
2013-12-26 11:25:42 +0100

    end of chapter 5, some dummy examples with flashBag and redirecting etc. Switched to NetBeans

--- src/Ssstrz/TestBundle/Controller/DefaultController.php
+++ src/Ssstrz/TestBundle/Controller/DefaultController.php
@@ -6,19 +6,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+	
     public function indexAction($name, $surname)
-    {
-    	$url = $this->generateUrl('ssstrz_test_homepage', array(
+    {    	
+    	if ($name === 'aneta') {
+            /*return $this->redirect(
+                            $this->generateUrl('acme_hello_homepage', array('name'=>$name))
+            );*/
+            return $this->forward('AcmeHelloBundle:Hello:index', array(
+                'name' => $name
+            ));	
+    	}
+        
+        if ($name === 'tomek') {
+            $this->get('session')->getFlashBag()->add(
+                'notice',
+                'master has come'
+            );
+        }
+        
+        
+   	$url = $this->generateUrl('ssstrz_test_homepage', array(
     		'name' => $name,
     		'surname' => $surname
     	));
+    	
         return $this->render(
-        		'SsstrzTestBundle:Default:index.html.twig', 
-        		array(
-        				'name' 		=> $name, 
-        				'surname' 	=> $surname, 
-        				'url' 		=> $url
-        		)
+            'SsstrzTestBundle:Default:index.html.twig', 
+            array(
+                'name' 		=> $name, 
+                'surname' 	=> $surname, 
+                'url' 		=> $url
+            )
         );
     }
 }
